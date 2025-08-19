be8fc5138a5d1ebfedb7c5e34d4d95e5e05684e9
StefanoDeSa <stefanobritodesa@gmail.com>
2025-08-19 20:12:01 -0300

    feat: adiciona opcao 1

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import module as m
 opcao = ""
 while opcao != "0":
     m.exibir_menu()
-    opcao = input("Opção: ")
+    opcao = input(">> Opção: ")
 
     if opcao == "1":
         print(">> Gerando novas chaves...\n")
+
+        info = m.gerar_e_salvar_chaves()
+        print(">> Chaves geradas com sucesso!")
+        print(f"   Privada: {info['private_key_path']}")
+        print(f"   Pública: {info['public_key_path']}")
+        
     elif opcao == "2":
         print(">> Encriptando mensagem...\n")
     elif opcao == "3":
--- module.py
+++ module.py
@@ -1,5 +1,8 @@
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from pathlib import Path
+import base64, hashlib
+
 
 def exibir_menu():
     print("==============================")
@@ -15,3 +18,38 @@ def gerar_par_rsa(bits=2048):
     priv = rsa.generate_private_key(public_exponent=65537, key_size=bits)
     pub = priv.public_key()
     return priv, pub
+
+def salvar_priv_pem(priv, caminho="rsa_priv.pem"):
+    with open(caminho, "wb") as f:
+        f.write(priv.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+
+def salvar_pub_pem(pub, caminho="rsa_pub.pem"):
+    with open(caminho, "wb") as f:
+        f.write(pub.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+
+def gerar_e_salvar_chaves(bits=2048, dir_keys="keys", nome_priv="rsa_priv.pem", nome_pub="rsa_pub.pem"):
+    """
+    Função orquestradora p/ usar no menu:
+    - Gera o par
+    - Salva PEM (privada e pública)
+    - Retorna caminhos das chaves
+    """
+    Path(dir_keys).mkdir(parents=True, exist_ok=True)
+    caminho_priv = str(Path(dir_keys) / nome_priv)
+    caminho_pub  = str(Path(dir_keys) / nome_pub)
+
+    priv, pub = gerar_par_rsa(bits=bits)
+    salvar_priv_pem(priv, caminho=caminho_priv)
+    salvar_pub_pem(pub, caminho=caminho_pub)
+
+    return {
+        "private_key_path": caminho_priv,
+        "public_key_path": caminho_pub,
+    }
